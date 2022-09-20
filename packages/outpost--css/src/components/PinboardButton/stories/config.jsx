9527210f4f5dc57b155366438780db83f649114f
Han <apricot13@users.noreply.github.com>
2022-09-20 15:12:35 +0100

    .

---
+++ packages/outpost--css/src/components/PinboardButton/stories/config.jsx
@@ -0,0 +1,14 @@
+export const title = 'PinboardButton';
+export const pathRoot = 'Components'; 
+
+export const config = {
+  title: `${pathRoot}/${title}`,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/tVVtW6tHgghun8aY261M5z/Outpost-platform-(and-Scout-etc)?node-id=138%3A2130'
+    },
+    layout: 'padded'
+  },
+}
+
---
+++ packages/outpost--css/src/components/ShareDialog/stories/config.jsx
@@ -0,0 +1,14 @@
+export const title = 'Share Dialog';
+export const pathRoot = 'Components'; 
+
+export const config = {
+  title: `${pathRoot}/${title}`,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/tVVtW6tHgghun8aY261M5z/Outpost-platform-(and-Scout-etc)?node-id=138%3A2132'
+    },
+    layout: 'padded'
+  },
+}
+
